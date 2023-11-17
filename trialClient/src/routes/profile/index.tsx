959f79d512b4bc59a9e18f0b412a7447b271ca0f
Isaac-George <isaacgeorge6@gmail.com>
2023-11-17 15:26:53 +0300

    revising code

--- trialClient/src/routes/profile/index.tsx
+++ trialClient/src/routes/profile/index.tsx
@@ -1,8 +1,29 @@
-import {component$} from "@builder.io/qwik";
+import {$, component$, useSignal, useTask$, useVisibleTask$} from "@builder.io/qwik";
 
-export default component$(()=>{
-    return <>
-    <p>hello</p>
+export default component$(() => {
+    const hours = useSignal<number>(0)
+    const minutes = useSignal<number>(0)
+    const seconds = useSignal<number>(0)
 
-    </>
+
+    useVisibleTask$(()=>{
+        const myDate = $(() => {
+            const fullDate = new Date();
+            hours.value = fullDate.getHours()
+            minutes.value = fullDate.getMinutes()
+            seconds.value = fullDate.getSeconds()
+        });
+        setInterval(myDate, 1000)
+    })
+
+
+
+
+
+    return (
+        <>
+            <p>{hours.value} :  {`${minutes.value < 9? "0" + minutes.value : minutes.value}` }: {seconds.value}</p>
+
+        </>
+    )
 })
